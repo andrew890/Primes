5c40405faf875e95872f9c964bb2c27a9c70dec9
Andrew Rowley <andrew@blackhillsoftware.com>
2021-06-24 01:04:24 -0500

    Create separate 31 and 64 bit builds

--- PrimeCPPzOS/solution_1/runzos.sh
+++ PrimeCPPzOS/solution_1/runzos.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-xlc -o PrimeCPP -O3 -Wl,lp64,xplink -Wc,xplink,-qlanglvl=extended0x PrimeCPP.cpp
-./PrimeCPP
+xlc -o PrimeCPP31 -O3 -Wl,xplink -Wc,xplink,-qlanglvl=extended0x PrimeCPP.cpp
+./PrimeCPP31
+
